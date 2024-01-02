6647d3da292f62d97e33f68b13925ab2e70125b3
LcuasLedesma <lucasfidel99@gmail.com>
2024-01-01 21:12:46 -0300

    table

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,12 +1,12 @@
+import { CurrencyPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [CurrencyPipe, RouterLink],
   templateUrl: './products.component.html',
-  styleUrl: './products.component.css'
+  styleUrl: './products.component.css',
 })
-export class ProductsComponent {
-
-}
+export class ProductsComponent {}
